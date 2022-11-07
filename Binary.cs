bb3433e45dd9b14ef2efe617f7c33a8d107aa406
dcast483 <117224455+dcast483@users.noreply.github.com>
2022-11-07 12:41:42 -0700

    Add files via upload

---
+++ Binary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertificationPrac
+{
+    internal class Binary: QuickSearch
+    {
+        static void Main(string[] args)
+        {
+            //Selected Sort
+            int[] arr = new int[20] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34, 11, 34, 64, 59, 2, 55, 36, 94, 74, 81 };
+            int n = 20;
+            Console.WriteLine("Selection sort");
+            Console.Write("Initial array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            //gets smallest num
+            int temp, smallest;
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+            Console.WriteLine();
+            Console.Write("Sorted array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+                
+            }
+            Console.ReadLine();
+            //Quick Search
+            int[] arrs = { 67, 12, 95, 56, 85, 1, 100, 23, 60, 9 };
+            int x = 10, z;
+            Console.WriteLine("Quick Sort");
+            Console.Write("Initial array is: ");
+            for (z = 0; z < x; z++)
+            {
+                Console.Write(arrs[z] + " ");
+            }
+
+            quickSort(arrs, 0, 9);
+            Console.Write("Sorted Array is: ");
+            for (z = 0; z < x; z++)
+            {
+                Console.Write(arrs[z] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSorts
+{
+    internal class Program
+    {
+        void merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            // Copy data to temp arrays
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            i = 0;
+            j = 0;
+
+            // Initial index of merged
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        // Main function
+        void sort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                // Find the middle
+                int m = l + (r - l) / 2;
+
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+
+                // Merge the sorted halves
+                merge(arr, l, m, r);
+            }
+        }
+
+        // A utility function to
+        // print array of size n */
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
+        // Driver code
+        public static void Main(String[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6, 7, 21, 26, 2, 15, 34, 57, 90, 81, 53, 64, 55, 77, 22, 35, 66, 68, 74, 82, 76 };
+            Console.WriteLine("Given Array");
+            printArray(arr);
+            Program ob = new Program();
+            ob.sort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\nSorted array");
+            printArray(arr);
+            Console.ReadLine();
+        }
+    }
+}
+
